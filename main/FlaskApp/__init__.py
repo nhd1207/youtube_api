7392f670ca72316931b635ce3de0d487866d2711
longnh-uit <longnh.0601@gmail.com>
2022-11-07 16:04:51 +0700

    config azure function

---
+++ main/FlaskApp/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from flask_cors import CORS, cross_origin
+
+import urllib.parse as p
+import re
+import os
+
+load_dotenv()
+MONGO_URI = os.getenv('MONGO_URI')
+app = Flask(__name__)
+CORS(app)
+client = MongoClient(MONGO_URI)
+
+db = client.youtube_db
+comments = db.comments
+
+# @app.get("/drop")
+# def drop():
+#     comments.drop()
+#     return("dropped")
+
+@app.post("/url")
+def save_youtube_url():
+    url = request.json.get("url")
+    pattern = '[a-zA-Z0-9:/.?]+'
+    id = re.findall(pattern, url)[1]
+    comments.update_one({"id": id}, {"$set": { "id": id, "url": url, "signal": "start" }}, upsert=True)
+    return("succeed" + url)
+
+if __name__ == "__main__":
+    app.run()
--- main/scrapeComments/__init__.py
+++ main/scrapeComments/__init__.py
@@ -1,8 +1,13 @@
 import logging
 
 import azure.functions as func
+from FlaskApp import app
+from Utils import export_comments
 
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    """Each request is redirected to the WSGI handler.
+    """
+    response = func.WsgiMiddleware(app.wsgi_app).handle(req, context)
+    export_comments(req.get_json().get("url"))
+    return response
 
-def main(documents: func.DocumentList) -> str:
-    if documents:
-        logging.info('Document id: %s', documents[0]['id'])
