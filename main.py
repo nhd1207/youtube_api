85c546de8245268bff2e4b663ad9e6d308559ae4
longnh-uit <longnh.0601@gmail.com>
2022-11-16 21:58:23 +0700

    adjust label value

--- main.py
+++ main.py
@@ -179,6 +179,8 @@ def predict():
 
     # In[19]:
 
+
+    label = {0:"0", 1:"1", 2:"2"}
     response = {
         "success": True,
         "result": []
@@ -193,7 +195,7 @@ def predict():
         content = blink_data[index]
 
     #   print(label[(text_model.predict(test_thu).argmax(axis=-1)[index])])
-        signed_label = (text_model.predict(test_thu).argmax(axis=-1)[index])
+        signed_label = label[(text_model.predict(test_thu).argmax(axis=-1)[index])]
         # f.write(f"{youtube_url},{content},{signed_label}\n")
         response["result"].append({"content": content, "label": signed_label})
     
