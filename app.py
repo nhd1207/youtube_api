4f4d267166d05bb4bf229dc04e41d67486157fc5
longnh-uit <longnh.0601@gmail.com>
2022-11-26 17:24:48 +0700

    init get result api

--- app.py
+++ app.py
@@ -1,233 +1,32 @@
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from flask_cors import CORS, cross_origin
-from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from pymongo import MongoClient
 
-import urllib.parse as p
 import re
 import os
 import pickle
 
 load_dotenv()
 app = Flask(__name__)
+MONGO_URI = os.getenv("MONGO_URI")
 CORS(app)
+client = MongoClient(MONGO_URI)
 
-# Azure blob
-# connect_str = os.getenv("BLOB_CONNECTION_STRING")
-# blob_container = os.getenv("CONTAINER")
-# blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+db = client.Youtube
+results = db.results
 
-SCOPES = ["https://www.googleapis.com/auth/youtube.force-ssl"]
-
-def youtube_authenticate():
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "credentials.json"
-    creds = None
-    # the file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first time
-    if os.path.exists("token.pickle"):
-        with open("token.pickle", "rb") as token:
-            creds = pickle.load(token)
-    # if there are no (valid) credentials availablle, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, SCOPES)
-            creds = flow.run_local_server(port=0)
-        # save the credentials for the next run
-        with open("token.pickle", "wb") as token:
-            pickle.dump(creds, token)
-
-    return build(api_service_name, api_version, credentials=creds)
-
-# authenticate to YouTube API
-youtube = youtube_authenticate()
-
-def get_video_id_by_url(url):
-    """
-    Return the Video ID from the video `url`
-    """
-    # split URL parts
-    parsed_url = p.urlparse(url)
-    # get the video ID by parsing the query of the URL
-    video_id = p.parse_qs(parsed_url.query).get("v")
-    if video_id:
-        return video_id[0]
-    else:
-        raise Exception(f"Wasn't able to parse video URL: {url}")
-
-def get_video_details(youtube, **kwargs):
-    return youtube.videos().list(
-        part="snippet,contentDetails,statistics",
-        **kwargs
-    ).execute()
-
-def print_video_infos(video_response):
-    items = video_response.get("items")[0]
-    # get the snippet, statistics & content details from the video response
-    snippet         = items["snippet"]
-    statistics      = items["statistics"]
-    content_details = items["contentDetails"]
-    # get infos from the snippet
-    channel_title = snippet["channelTitle"]
-    title         = snippet["title"]
-    description   = snippet["description"]
-    publish_time  = snippet["publishedAt"]
-    # get stats infos
-    comment_count = statistics["commentCount"]
-    like_count    = statistics["likeCount"]
-    view_count    = statistics["viewCount"]
-    # get duration from content details
-    duration = content_details["duration"]
-    # duration in the form of something like 'PT5H50M15S'
-    # parsing it to be something like '5:50:15'
-    parsed_duration = re.search(f"PT(\d+H)?(\d+M)?(\d+S)", duration).groups()
-    duration_str = ""
-    for d in parsed_duration:
-        if d:
-            duration_str += f"{d[:-1]}:"
-    duration_str = duration_str.strip(":")
-    print(f"""\
-    Title: {title}
-    Description: {description}
-    Channel Title: {channel_title}
-    Publish time: {publish_time}
-    Duration: {duration_str}
-    Number of comments: {comment_count}
-    Number of likes: {like_count}
-    Number of views: {view_count}
-    """)
-
-def search(youtube, **kwargs):
-    return youtube.search().list(
-        part="snippet",
-        **kwargs
-    ).execute()
-
-def parse_channel_url(url):
-    """
-    This function takes channel `url` to check whether it includes a
-    channel ID, user ID or channel name
-    """
-    path = p.urlparse(url).path
-    id = path.split("/")[-1]
-    if "/c/" in path:
-        return "c", id
-    elif "/channel/" in path:
-        return "channel", id
-    elif "/user/" in path:
-        return "user", id
-
-def get_channel_id_by_url(youtube, url):
-    """
-    Returns channel ID of a given `id` and `method`
-    - `method` (str): can be 'c', 'channel', 'user'
-    - `id` (str): if method is 'c', then `id` is display name
-        if method is 'channel', then it's channel id
-        if method is 'user', then it's username
-    """
-    # parse the channel URL
-    method, id = parse_channel_url(url)
-    if method == "channel":
-        # if it's a channel ID, then just return it
-        return id
-    elif method == "user":
-        # if it's a user ID, make a request to get the channel ID
-        response = get_channel_details(youtube, forUsername=id)
-        items = response.get("items")
-        if items:
-            channel_id = items[0].get("id")
-            return channel_id
-    elif method == "c":
-        # if it's a channel name, search for the channel using the name
-        # may be inaccurate
-        response = search(youtube, q=id, maxResults=1)
-        items = response.get("items")
-        if items:
-            channel_id = items[0]["snippet"]["channelId"]
-            return channel_id
-    raise Exception(f"Cannot find ID:{id} with {method} method")
-
-def get_channel_videos(youtube, **kwargs):
-    return youtube.search().list(
-        **kwargs
-    ).execute()
-
-
-def get_channel_details(youtube, **kwargs):
-    return youtube.channels().list(
-        part="statistics,snippet,contentDetails",
-        **kwargs
-    ).execute()
-
-def get_comments(youtube, **kwargs):
-    return youtube.commentThreads().list(
-        part="snippet",
-        **kwargs
-    ).execute()
-
-@app.get("/scrape")
-def scrape():
-    args = request.args
-    video_id = ""
-    url = args.get("url")
-    if "watch" in url:
-        video_id = get_video_id_by_url(url)
-        params = {
-            'videoId': video_id,
-            'order': 'relevance'
-        }
-
-    else:
-        return("not a video")
-    youtube_video = { 
-        "youtube_url": url,
-        "name": get_video_details(youtube, id=video_id).get("items")[0]["snippet"]["title"],
-        "comments": []
+@app.get("/result")
+def result():
+    url = request.args.get("url")
+    response = {
+        "success": True,
+        "data": []
     }
-    pattern = '[a-zA-Z0-9:/.?]+'
-    get_id = re.findall(pattern, url)[1]
-    file_name = f"youtube_url_{get_id}.csv"
-    resObject = { "file_name": file_name, "youtube_url": url, "comments": [] }
+    for document in results.find({ "youtube_url": url }):
+        comment = document["content"]
+        label_id = document["label_id"]
+        response["data"].append({ "comment": comment, "label_id": label_id})
 
-    while True:
-        # make API call to get all comments from the channel (including posts & videos)
-        response = get_comments(youtube, **params)
-        items = response.get("items")
-        # if items is empty, breakout of the loop
-        if not items:
-            break
-        # print(items[1]["snippet"]["topLevelComment"]["snippet"])
-        for item in items:
-            comment = item["snippet"]["topLevelComment"]["snippet"]["textDisplay"].replace("\r", " ")
-            comment = comment.replace(",", "&#44;")
-
-            # f.write(f"""
-            # Author: {author}
-            # Comment: {comment}
-            # Likes: {like_count}
-            # Updated At: {updated_at}
-            # ==================================\n
-            # """)
-
-            resObject["comments"].append(comment)
-        if "nextPageToken" in response:
-            # if there is a next page
-            # add next page token to the params we pass to the function
-            params["pageToken"] =  response["nextPageToken"]
-        else:
-            # must be end of comments!!!!
-            break
-
-    # blob_client = blob_service_client.get_blob_client(container=blob_container, blob=file_name)
-
-    # with open(file=f"./output/{file_name}", mode="rb") as data:
-    #     blob_client.upload_blob(data, overwrite=True)
-    return resObject
+    if len(response["data"]) == 0: return {"success": False, "message": "data not found"}, 404
+    return response
